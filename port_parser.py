1c1d1300e8f41a613655ed7fb694d2a42cbe5e75
gorillaj <johnnyloneal@gmail.com>
2023-10-04 13:17:22 -0400

    better filtering method, still not correct results

--- port_parser.py
+++ port_parser.py
@@ -2,23 +2,15 @@
 import re
 
 # Need a better regex expression to filter out special characters.  Perhaps a split is the way to do this.
-logfile = open("caterpillar_log.txt") #default mode is read only
+logfile = ("caterpillar_log.txt") #default mode is read only
 pattern = r"([^`~!@#$%^&*()_+={}\[\]|\\:;“’<,>.?๐฿A-Za-z](?:[0-9]+(?:-[0-9]+)?)(?:,[0-9]+(?:-[0-9]+)?)*)" #pattern for port number 0-65535
-port_list = []
+with open(logfile) as f:
+    for line in f:
+        num_pattern=re.findall( '(\d+)', line ) #filter out nondigits
+        for item in num_pattern:
+            numbers=re.search(pattern,item)  #need to filter for str lengths greater than 5
+            print(numbers.group()) #is group needed here?
 
-for line in logfile:
-    # this could surely be consolidated to a function at the least
-    ports = re.findall(pattern,line)
-    for port in ports:
-        if port.isdigit() == False: #port number is numeric
-            pass
-        if len(port) > 5: 
-            pass   #port number can be no more than 5 digits
-        if port[0] == 0 and len(line) > 1:
-            pass
-        port_list.append(ports)
-    
-        print(port_list)
 
 
 
