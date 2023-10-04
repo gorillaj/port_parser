c11edda27b01e40b2b5185d3e9cdd1ed9865fc69
gorillaj <johnnyloneal@gmail.com>
2023-10-04 14:29:03 -0400

    better parsing method

--- port_parser.py
+++ port_parser.py
@@ -3,13 +3,13 @@ import re
 
 # Need a better regex expression to filter out special characters.  Perhaps a split is the way to do this.
 logfile = ("caterpillar_log.txt") #default mode is read only
-pattern = r"([^`~!@#$%^&*()_+={}\[\]|\\:;“’<,>.?๐฿A-Za-z](?:[0-9]+(?:-[0-9]+)?)(?:,[0-9]+(?:-[0-9]+)?)*)" #pattern for port number 0-65535
+pattern = r"^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$" #pattern for port number 0-65535
 with open(logfile) as f:
-    for line in f:
-        num_pattern=re.findall( '(\d+)', line )
+    for line in f:                      #log files should be opened line by line
+        num_pattern=re.findall('(\d{1,5})', line) #find number strings len<5 
         for item in num_pattern:
-            numbers=re.findall(pattern,item)
-            print(numbers)
+            numbers=re.search(pattern,item)  
+            print(numbers.group())
         
 
 
