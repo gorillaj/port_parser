0e15f2902f09209e6e797a151231dbe9c8d3ec8f
gorillaj <johnnyloneal@gmail.com>
2023-10-05 13:15:10 -0400

    added sys module and sys.arg (disabled)

--- port_parser.py
+++ port_parser.py
@@ -1,8 +1,9 @@
 #! usr/bin/env/python3
 
+#import sys module would be used for most cases
 import re
 
-logfile = ("caterpillar_log.txt") #default mode is read only
+logfile = ("caterpillar_log.txt") # this would be sys.argv[1] for most use cases #default mode is read only
 pattern = r"^ 0|([1-5][0-9]{0,4})|(6[1-4,0][0-9]{0,3})|(65[1-4,0][0-9]{0,2})|(655[1-2,0][0-9])|(6553[1-5,0])$ " #pattern for port number 0-65535
 filtered_items = r"[A-Za-z],\./,\-/)" # Need a better regex expression to filter out special characters.
 dates = r"^\d{2}[/-]\d{2}[/-]\d{4}$" #mm/dd/yyyy
@@ -28,7 +29,7 @@ with open(logfile) as f:
                                 ports.add(port)
                                 # ports[port] = ports.get (item, 0) +1                        #output unique values for port no
 print(*ports,sep = "\n")        #additional processing would reveal only certain ports but could overlook potential ones
-                            
+                          
                                
 
 
