8d0371dc4aa7e3e0ee019c24bac1d645273d679f
gorillaj <johnnyloneal@gmail.com>
2023-10-05 12:44:21 -0400

    Add unique filtering and output

--- port_parser.py
+++ port_parser.py
@@ -1,17 +1,17 @@
 #! usr/bin/env/python3
-import re
-import numpy as np
 
-# Need a better regex expression to filter out special characters.  
+import re
+
 logfile = ("caterpillar_log.txt") #default mode is read only
 pattern = r"^ 0|([1-5][0-9]{0,4})|(6[1-4,0][0-9]{0,3})|(65[1-4,0][0-9]{0,2})|(655[1-2,0][0-9])|(6553[1-5,0])$ " #pattern for port number 0-65535
-filtered_items = r"[A-Za-z],\./,\-/)" # Need a better regex expression to filter out special characters
+filtered_items = r"[A-Za-z],\./,\-/)" # Need a better regex expression to filter out special characters.
 dates = r"^\d{2}[/-]\d{2}[/-]\d{4}$" #mm/dd/yyyy
 alt_dates = r"^\d{4}-\d{2}-\d{2}$"  #yyyy/dd/mm
 timestamps = r"^\d{2}:\d{2}:\d{2}$"
-ip_addresses =r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
+ip_addresses  =r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
 cleaned_strings=[]
-all_ports=[]
+all_ports =[]
+ports = set()
 
 with open(logfile) as f:
     for line in f:                      #log files should be opened line by line
@@ -24,10 +24,12 @@ with open(logfile) as f:
                     for item in possible_ports:
                            if len(item) < 6 and item.isdigit() == True:
                             all_ports.append(item)
+                            for port in all_ports:
+                                ports.add(port)
+                                # ports[port] = ports.get (item, 0) +1                        #output unique values for port no
+print(*ports,sep = "\n")        #additional processing would reveal only certain ports but could overlook potential ones
                             
-                            ports=np.array(all_ports)          #output unique values for port no
-                            port_no=np.unique(ports)
-                            print(port_no)   
+                               
 
 
 
