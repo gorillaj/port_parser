ca30bebbd4a901b52c56cdaf481ffe26116e6081
gorillaj <johnnyloneal@gmail.com>
2023-10-05 03:12:16 -0400

    Changed regex expressions, filtered date/time/ip, want unique output numbers.
    Number outputs are valid port numbers although I think more than original
    log presented.  However, changing without better parsing method would compromise utility for other logs

--- port_parser.py
+++ port_parser.py
@@ -1,16 +1,33 @@
 #! usr/bin/env/python3
 import re
+import numpy as np
 
-# Need a better regex expression to filter out special characters.  Perhaps a split is the way to do this.
+# Need a better regex expression to filter out special characters.  
 logfile = ("caterpillar_log.txt") #default mode is read only
-pattern = r"^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$" #pattern for port number 0-65535
+pattern = r"^ 0|([1-5][0-9]{0,4})|(6[1-4,0][0-9]{0,3})|(65[1-4,0][0-9]{0,2})|(655[1-2,0][0-9])|(6553[1-5,0])$ " #pattern for port number 0-65535
+filtered_items = r"[A-Za-z],\./,\-/)" # Need a better regex expression to filter out special characters
+dates = r"^\d{2}[/-]\d{2}[/-]\d{4}$" #mm/dd/yyyy
+alt_dates = r"^\d{4}-\d{2}-\d{2}$"  #yyyy/dd/mm
+timestamps = r"^\d{2}:\d{2}:\d{2}$"
+ip_addresses =r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
+cleaned_strings=[]
+all_ports=[]
+
 with open(logfile) as f:
     for line in f:                      #log files should be opened line by line
-        num_pattern=re.findall('(\d{1,5})', line) #find number strings len<5 
-        for item in num_pattern:
-            numbers=re.search(pattern,item)  
-            print(numbers.group())
-        
+        lines=line.split() 
+        for line in lines:
+            if line is not re.findall(dates,line) and line is not re.findall(timestamps,line) and line is not re.findall(ip_addresses,line) and line is not re.findall(alt_dates,line):   #remove dates, timestamps, and ip addresses
+                cleaned_strings.append(line)
+                for line in cleaned_strings:
+                    possible_ports=line.split('"')
+                    for item in possible_ports:
+                           if len(item) < 6 and item.isdigit() == True:
+                            all_ports.append(item)
+                            
+                            ports=np.array(all_ports)          #output unique values for port no
+                            port_no=np.unique(ports)
+                            print(port_no)   
 
 
 
